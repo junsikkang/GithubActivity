4e6d84cd2d2ed19117799c19b6f9260b8d092323
junsikkang <junkang@ucdavis.edu>
2017-12-07 10:17:56 -0800

    Add sleep to autoswap.sh

--- hw4/autoswap.sh
+++ hw4/autoswap.sh
@@ -5,21 +5,21 @@ docker ps -a > /tmp/yy_xx$$
 if grep --quiet web1 /tmp/yy_xx$$
   then
   echo "Running container web2"
-  docker run -d -P --name web2 $1
+  docker run -d -P --name web2 $1 && sleep 3
   echo "Connecting web2 to network"
-  docker network connect ecs189_default web2
+  docker network connect ecs189_default web2 && sleep 3
   echo "Executing swap2 in the proxy container"
-  docker exec ecs189_proxy_1 /bin/bash /bin/swap2.sh
+  docker exec ecs189_proxy_1 /bin/bash /bin/swap2.sh && sleep 3
   echo "Killing web1"
   docker rm -f `docker ps -a | grep web1 | sed -e 's: .*$::'`
 elif grep --quiet web2 /tmp/yy_xx$$
   then
   echo "Running container web1"
-  docker run -d -P --name web1 $1
+  docker run -d -P --name web1 $1 && sleep 3
   echo "Connecting web1 to network"
-  docker network connect ecs189_default web1
+  docker network connect ecs189_default web1 && sleep 3
   echo "Executing swap1 in the proxy container"
-  docker exec ecs189_proxy_1 /bin/bash /bin/swap1.sh
+  docker exec ecs189_proxy_1 /bin/bash /bin/swap1.sh && sleep 3
   echo "Killing web2"
   docker rm -f `docker ps -a | grep web2 | sed -e 's: .*$::'`
 fi
